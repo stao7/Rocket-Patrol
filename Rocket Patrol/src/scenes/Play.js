2acc75f6ac59b8eb5cda85ffd26b0b2d7dfcf278
stao7 <stao7@ucsc.edu>
2022-04-13 18:55:14 -0700

    push1

--- Rocket Patrol/src/scenes/Play.js
+++ Rocket Patrol/src/scenes/Play.js
@@ -11,7 +11,6 @@ class Play extends Phaser.Scene{
     create(){
         // place tile sprite
         this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
-        this.add.text(20, 20, "Rocket Patrol Play")
         // green UI background
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
         // white borders
